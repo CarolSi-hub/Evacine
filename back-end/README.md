019ddec0d39fe92a552513d456784075f64477d7
CarolSi <a.carolg1@gmail.com>
2021-03-29 11:08:16 -0300

    comeco de form para cadastro

--- back-end/README.md
+++
@@ -1,5 +0,0 @@
-# Evacine
-
-Java + React project (Postgre local Database)
-
-Evacine is an app to signup people to get a vacine.
--- front-end/evacine-web/src/App.js
+++ front-end/evacine-web/src/App.js
@@ -1,31 +1,6 @@
-import { Layout, Menu } from 'antd';
-import ListaPessoas from './components/ListaPessoas';
-import 'antd/dist/antd.css';
+import React from 'react';
+import Routes from './routes';
 
-const { Header, Content, Footer } = Layout;
-export default function App() {
-  return (
-    <div>
-     <Layout className="layout">
-      <Header>
-        <div className="logo">
-          <Menu 
-          theme="dark" 
-          mode="horizontal"
-          defaultSelectedKeys={['1']} >
-            <Menu.Item key="1">Home</Menu.Item>
-          </Menu>
-        </div>
-      </Header>
-      <Content style={{padding: '0 50px'}}>
-        <div className="site-layout-content">
-          <ListaPessoas></ListaPessoas>
-        </div>
-      </Content>
-      <Footer style={{textAlign: 'center'}}>
-        Evacine
-      </Footer>
-     </Layout>
-    </div>
-  );
-}
+const App = () => <Routes />;
+
+export default App;
